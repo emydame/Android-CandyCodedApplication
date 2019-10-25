3fa2752db9f9f2714fba815107e3d20cd057cb3e
Emilia <8481164+emydame@users.noreply.github.com>
2019-10-25 07:58:03 +0100

    Starting the phone activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -6,17 +6,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-public class InfoActivity extends AppCompatActivity {
+import static android.net.Uri.*;
 
+public class InfoActivity extends AppCompatActivity {
+    TextView textViewPhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        textViewPhone = (TextView) findViewById(R.id.text_view_phone);
 
-        Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
+        Uri uri = parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
         ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
@@ -30,7 +34,7 @@ public class InfoActivity extends AppCompatActivity {
     // ***
 
     public void createMapIntent( View view){
-        Uri uriAddress = Uri.parse( "geo:0,0?q=618 E South St Orlando, FL 32801" );
+        Uri uriAddress = parse( "geo:0,0?q=618 E South St Orlando, FL 32801" );
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
         mapIntent.setPackage("com.google.android.apps.maps");
         if(mapIntent.resolveActivity(getPackageManager()) != null){
@@ -42,4 +46,19 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneItntent(View view) {
+        Intent intentCall = new Intent(Intent.ACTION_DIAL);
+        intentCall.setData(Uri.parse("tel:0123456789"));
+        startActivity(intentCall);
+    }
+
+    public void createPhoneIntent( View view){
+
+
+        Intent intentCall = new Intent(Intent.ACTION_DIAL);
+        intentCall.setData(Uri.parse("tel:0123456789"));
+        startActivity(intentCall);
+
+    }
+
 }
